3936cff0e48ca3942572275fd5ce4c2db4881edd
a036920022002 <104316821+a036920022002@users.noreply.github.com>
2026-01-12 17:03:09 +0800

    Update aboutme.vue

--- src/views/pages/aboutme.vue
+++ src/views/pages/aboutme.vue
@@ -13,6 +13,7 @@ const envHost = import.meta.env.VITE_API_URL;
 onMounted(async () => {
     const res = await api.get('/api/Aboutme',{ requireAuth: false });
     const resCer = await api.get('/api/Certificate',{ requireAuth: false });
+    console.log("Aboutme Data:", res.data);
     
     if (Array.isArray(res.data)) {
             aboutme.value = res.data.map(item => ({
